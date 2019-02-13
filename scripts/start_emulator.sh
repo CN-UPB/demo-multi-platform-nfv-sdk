4607344a8f9c4bae7691bcc6de5529ef781b2a52
peusterm <manuel.peuster@uni-paderborn.de>
2019-02-13 14:07:16 +0100

    Added helper scripts
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

---
+++ scripts/start_emulator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run --name vim-emu -t -d --rm --privileged --pid='host' --network=netosm -v /var/run/docker.sock:/var/run/docker.sock vim-emu-img python examples/osm_default_daemon_topology_2_pop.py
+
+sleep 2
+
+export VIMEMU_HOSTNAME=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vim-emu)
+
+echo $VIMEMU_HOSTNAME
+
+osm vim-create --name emu-vim1 --user username --password password --auth_url http://$VIMEMU_HOSTNAME:6001/v2.0 --tenant tenantName --account_type openstack
---
+++ scripts/stop_emulator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+osm vim-delete emu-vim1
+
+sleep 2
+docker rm -f vim-emu
